b840c4351a2f5f0a6001c2b03527fc7bbb6f850e
koki-y <kokiydp@gmail.com>
2018-12-04 00:14:15 +0900

    add 2 files for practicing.

---
+++ chapter 7/my_list.exs
@@ -0,0 +1,36 @@
+defmodule MyList do
+  # Practice-0
+  # Sum without accumulator.
+  def sum(list), do: _sum(list)
+
+  defp _sum([]),            do: 0
+  defp _sum([head | tail]), do: head + _sum(tail)
+
+  # Practice-1
+  def mapsum(list, func), do: _mapsum(list, 0, func)
+
+  defp _mapsum([], value, _func), do: value
+  defp _mapsum([head | tail], value, func) do
+    _mapsum(tail, value+func.(head), func)
+  end
+
+  # Practice-2
+  def max(list), do: _max(list, 0)
+
+  defp _max([], value), do: value
+  defp _max([head | tail], value)
+    when head > value,  do: _max(tail, head)
+  defp _max([head | tail], value)
+    when head <= value, do: _max(tail, value)
+
+  # Practice-3
+  def ceasar(list, n), do: _ceasar(list, [], n)
+
+  defp _ceasar([], value, _n), do: value
+  defp _ceasar([head | tail], value, n) when head + n > 122  do
+    _ceasar(tail, value ++ [97 + rem(head + n, 123)], n)
+  end
+  defp _ceasar([head | tail], value, n) when head + n <= 122 do
+    _ceasar(tail, value ++ [head + n], n)
+  end
+end
