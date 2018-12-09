e32da66faf92e0a17d39f76c82271364ff0c1c28
koki-y <kokiydp@gmail.com>
2018-12-09 10:50:02 +0900

    Write D_Christmas answer in Elixir

---
+++ atcoder/D_Christmas.ex
@@ -0,0 +1,19 @@
+defmodule RunRun do
+  def feed(_, 0), do: 0
+  def feed(dimension, appetite) do
+    amount_of_p = round(:math.pow 2, (dimension + 1)) - 1
+    burger_size = round(:math.pow 2, (dimension + 2)) - 3
+    cond do
+      appetite < burger_size ->
+        feed(dimension - 1 , appetite - 1)
+      appetite > burger_size ->
+        amount_of_p + 1 + feed(dimension, appetite - burger_size - 1)
+      true ->
+        amount_of_p + 1
+    end
+  end
+end
+
+[x, n] = IO.read(:line) |> String.replace("\r", "") |> String.replace("\n", "")
+                        |> String.split(" ") |> Enum.map(&String.to_integer/1)
+IO.puts RunRun.feed x, n
