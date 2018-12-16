d7143f700eae88d68503da424fd273ff105a2ae3
koki-y <kokiydp@gmail.com>
2018-12-16 23:06:03 +0900

    add sorting method

--- chapter 13/issues/lib/issues/cli.ex
+++ chapter 13/issues/lib/issues/cli.ex
@@ -41,10 +41,12 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_maps
+    |> sort_into_ascending_order
+    |> Enum.take(count)
   end
 
   def decode_response({:ok, body}), do: body
@@ -58,5 +60,9 @@ defmodule Issues.CLI do
   def convert_to_list_of_maps(list) do
     list
     |> Enum.map(&Enum.into(&1, Map.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 end
